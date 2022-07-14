1e38b98acf6879af21729705e5d7192aadd1c0f7
jaybell <jaycooperbell@gmail.com>
2022-07-13 18:16:25 -0700

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'configurable-counter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
